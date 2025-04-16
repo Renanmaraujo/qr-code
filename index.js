c152950dc6ff9ee0b1d09cba44af4bf9a8a828c3
Renan Araujo <renanmoreira123@hotmail.com>
2025-04-16 10:21:55 -0300

    Subindo scraper de QR Code

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/get-qrcode', async (req, res) => {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox'],
+    headless: 'new',
+  });
+
+  const page = await browser.newPage();
+  await page.goto('https://evo.valparaiso.pro/instance/connect/REnan', {
+    waitUntil: 'networkidle2'
+  });
+
+  await page.waitForSelector('canvas');
+
+  const qrCodeBase64 = await page.evaluate(() => {
+    const canvas = document.querySelector('canvas');
+    return canvas.toDataURL('image/png');
+  });
+
+  await browser.close();
+
+  res.json({ base64: qrCodeBase64 });
+});
+
+app.get('/', (req, res) => {
+  res.send('Servidor QR Code online!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
